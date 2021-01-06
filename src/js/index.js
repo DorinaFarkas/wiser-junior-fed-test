23d162044b4542e4c79abb85de7618f90fb557b3
Dorina Farkas <farkas.dorina@outlook.com>
2021-01-06 21:21:49 +0000

    Completed task

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("ready");
 });
+
+toggleCard = () => {
+  const Card = document.querySelector(".card");
+  const buttonText = document.querySelector(".button-text");
+  
+  Card.classList.toggle("open");
+
+  if (buttonText.innerHTML === "Show description") {
+    buttonText.innerHTML = "Hide description";
+  } else {
+    buttonText.innerHTML = "Show description";
+  }
+};
